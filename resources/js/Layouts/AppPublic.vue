7988be8374967b895ef66eef907f95e30610a910
Angelo Schuler Piletti <74366173+AngeloSchulerPiletti@users.noreply.github.com>
2021-07-29 11:26:53 -0300

    project started

---
+++ resources/js/Layouts/AppPublic.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <!-- HEADER  -->
+  </div>
+  <main>
+      <slot></slot>
+  </main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/Pages/public/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <app-public>
+      OLA
+  </app-public>
+</template>
+
+<script>
+import AppPublic from "@/Layouts/AppPublic";
+export default {
+    components:{
+        AppPublic
+    }
+}
+</script>
+
+<style>
+
+</style>
